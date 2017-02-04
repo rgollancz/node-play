425b92b8f5ea1f446dfa9401ccce7f3de288c256
Rosie <r.gollancz@gmail.com>
2017-02-04 15:43:39 +0000

    Add Express

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-require('./app/index')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+  response.send('Hello from Express!')
+})
+
+app.get('/yo', (request, response) => {
+  response.send('Yo')
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
